7f8f60aadbc67290e2fdbe684b0127df874c0f12
ddao19 <dung.dao19@outlook.com>
2026-05-24 19:30:57 -0500

    fixed the rank variables

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -7,8 +7,8 @@ const Rank = ({ user }) => {
   
   return (
     <div className="rank-container">
-      <h2>{user.firstname}, your current entry count is...</h2>
-      <h2 className="rank-entries">#{user.entries}</h2>
+      <h2>{user[0].firstname}, your current entry count is...</h2>
+      <h2 className="rank-entries">#{user[0].entries}</h2>
     </div>
   )
 }
